c895776c6352f663151e475db6ae46530968a993
nomansadiq11 <nomi.cs786@gmail.com>
2022-03-05 01:05:48 +0400

    cluster added

--- outputs.tf
+++ outputs.tf
@@ -49,3 +49,37 @@ output "worker-subnet-name" {
 output "worker-subnet-OCID" {
   value = oci_core_subnet.vcn-subnet-worker.id
 }
+
+
+# Outputs for k8s cluster
+
+output "cluster-name" {
+  value = oci_containerengine_cluster.oke-cluster.name
+}
+output "cluster-OCID" {
+  value = oci_containerengine_cluster.oke-cluster.id
+}
+output "cluster-kubernetes-version" {
+  value = oci_containerengine_cluster.oke-cluster.kubernetes_version
+}
+output "cluster-state" {
+  value = oci_containerengine_cluster.oke-cluster.state
+}
+
+# Outputs for k8s node pool
+
+output "node-pool-name" {
+  value = oci_containerengine_node_pool.oke-node-pool.name
+}
+output "node-pool-OCID" {
+  value = oci_containerengine_node_pool.oke-node-pool.id
+}
+output "node-pool-kubernetes-version" {
+  value = oci_containerengine_node_pool.oke-node-pool.kubernetes_version
+}
+output "node-size" {
+  value = oci_containerengine_node_pool.oke-node-pool.node_config_details[0].size
+}
+output "node-shape" {
+  value = oci_containerengine_node_pool.oke-node-pool.node_shape
+}
